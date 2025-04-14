2399a66f79247bfa4f94893d8b6624e11bf350d4
Rodig0SantOs <rodrigo.santos@stwbrasil.com>
2025-04-14 12:51:20 -0300

    ajuste variaveis

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,15 +1,14 @@
 // emailService.js
 import emailjs from "@emailjs/browser";
 
+// Inicialização única (não dentro de sendEmail)
+emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+
 export const sendEmail = async (formData) => {
   try {
-    console.log("Public Key:", import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
-    console.log("Service ID:", import.meta.env.VITE_EMAILJS_SERVICE_ID);
-    console.log("Template ID:", import.meta.env.VITE_EMAILJS_TEMPLATE_ID);
-
     const templateParams = {
-      from_name: formData.nome || "Sistema de Ocorrências",
-      subject: `Registro de Ocorrência - ${formData.nome || "Anônimo"}`,
+      from_name: formData.nome,
+      subject: `Registro de Ocorrência - ${formData.nome}`,
       email: formData.email || "Não informado",
       reply_to: formData.email || "",
       incident_date: formData.data_hora || "Não informado",
@@ -23,18 +22,17 @@ export const sendEmail = async (formData) => {
       /* anexo: anexosFormatados, */
     };
 
-    const serviceID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-    const templateID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-
     const response = await emailjs.send(
-      serviceID,
-      templateID,
-      templateParams,
-      import.meta.env.VITE_EMAILJS_PUBLIC_KEY // adicione isso!
+      import.meta.env.VITE_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      templateParams
     );
+
+    console.log(response);
+
     return response;
   } catch (error) {
-    console.error("Erro detalhado no envio:", error);
-    throw new Error("Falha ao enviar e-mail: ", error.message);
+    console.error("Erro no envio:", error);
+    throw error;
   }
 };
